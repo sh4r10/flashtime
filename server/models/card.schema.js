9bc363569aa9ed41253cd35e5b9699915784a528
Umar Mahmood <umarma@net.chalmers.se>
2022-09-12 21:50:57 +0200

    add card schema back

---
+++ server/models/card.schema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const cardSchema = new Schema(
+  {
+    deckID: { type: Schema.Types.ObjectId, ref: 'User' },
+    front: { type: String, required: true },
+    back: { type: String, required: true },
+    lastRevision: { type: Date },
+    nextRevision: { type: Date },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const Card = mongoose.models('Card', cardSchema)
+module.exports = Card
