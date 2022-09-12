cfb0b44b1b0f7b240ea9e452fda90591647226b4
Alaa Taleb <alaat@student.chalmers.se>
2022-09-12 17:22:10 +0200

    create controller folder create controller folder for mangeging routes for decks, decksCollection and users imported the routes to the main app file
    :wq

---
+++ server/controller/deckController.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+router.get('/', (res, req) => {})
+
+router.get('/:id', (res, req) => {})
+
+router.post('/', (res, req) => {})
+
+router.put('/:id', (res, req) => {})
+
+router.delete('/:id', (res, req) => {})
