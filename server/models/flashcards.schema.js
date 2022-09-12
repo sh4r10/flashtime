5ee554e202a89cdc8a8e43eb0a48e059d8e50e96
Umar Mahmood <umarma@net.chalmers.se>
2022-09-12 16:16:37 +0200

    change the attributes name

--- server/models/flashcards.schema.js
+++ server/models/flashcards.schema.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const flashCards = new Schema({
-    decks_ID: {type: Schema.Types.ObjectId, ref:'User'},
+    deckID: {type: Schema.Types.ObjectId, ref:'User'},
     front: {type: String , req: true },
     back: {type: String , req: true },
-    lastRevise:  { type: Date },
+    lastRevision:  { type: Date },
     nextRevision: { type: Date } 
 
 },
 {
     timestamps : true,
 });
-const flashCardsSchema = mongoose.models('FlashCards',flashCards);
+const flashCardsSchema = mongoose.models('FlashCards', flashCards);
