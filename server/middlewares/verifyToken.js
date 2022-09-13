f64ab720d117ac8a7b8f17cb7e1293ac94de69c5
Shariq Shahbaz <iamsh4r10@gmail.com>
2022-09-13 18:14:01 +0200

    fix verifyToken middleware

--- server/middlewares/verifyToken.js
+++ server/middlewares/verifyToken.js
@@ -1,16 +1,17 @@
+const Users = require('../models/user.schema')
 const jwt = require('jsonwebtoken')
-const User = require('../models/user.schema')
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.header('authorization')
+  const authHeader = req.headers['authorization']
   const token = authHeader && authHeader.split(' ')[1]
-  if (!token) return res.status(401).json({ message: 'Access denied' })
-  JsonWebTokenError.verify(
+  if (!token) return res.sendStatus(401)
+  //verify access token
+  jwt.verify(
     token,
-    process.env.TOKEN_SECRET,
-    async (err, user) => {
-      if (err) return res.status(403).json({ message: 'Forbidden access.' })
-      req.user = await User.findById(user._id)
+    process.env.ACCESS_TOKEN_SECRET,
+    async (err, accessTokenData) => {
+      if (err) return res.sendStatus(403)
+      req.user = await Users.findById(accessTokenData.id)
       next()
     }
   )
