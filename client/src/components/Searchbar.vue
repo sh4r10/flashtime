295e17cf75f0d585a04fc27220670f8a8c74d226
Umar Mahmood <umarma@net.chalmers.se>
2022-09-27 23:12:01 +0200

    add the deckbox & searchbar components

---
+++ client/src/components/Searchbar.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input type="text" placeholder="Search for deck and decks collections..." >
+    </div>
+</template>
+<script>
+export default {
+  name: 'Searchbar'
+}
+</script>
+<style scoped>
+input {
+  display: inline-block;
+  width: 75%;
+  padding: 15px;
+  color:#313131;
+  font-size:20px;
+  appearance: none;
+  margin-top: 20%;
+  border:none;
+  background: none;
+  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
+  background-color: rgba(225,225,225,0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+input {
+  box-shadow: 0px 0px 16px rgba(234, 6, 6, 0.25); ;
+  background-color: rgba(225,225,225,0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+</style>
--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -1,60 +1,39 @@
 <template>
 <div>
-  <input type="text" placeholder="Search for deck and decks collections..." >
+  <Searchbar/>
   <h1>Need Revision</h1>
+  <h2>Your Collections </h2>
+  <Deckbox/>
 </div>
 </template>
 
 <script>
+import Searchbar from '../components/Searchbar.vue'
+import Deckbox from '../components/Deckbox.vue'
 export default {
   name: 'User',
   data() {
     return {
-      decks: [
-        {
-          id: 1,
-          name: 'OOP'
-
-        },
-        {
-          id: 2,
-          name: 'Math'
-        },
-        {
-          id: 3,
-          name: 'Psy'
-        }
-      ],
-      deckCollection: []
     }
   },
   methods: {},
-  created: {}
+  created: {},
+  components: {
+    Searchbar,
+    Deckbox
+  }
 }
 </script>
+
 <style scoped>
-  input {
-  display: inline-block;
-  width: 75%;
-  padding: 15px;
-  color:#313131;
-  font-size:20px;
-  appearance: none;
-  margin-top: 20%;
-  border:none;
-  background: none;
-  box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
-  background-color: rgba(225,225,225,0.5);
-  border-radius: 0px 16px 0px 16px;
-  transition: 0.4s;
-}
-input {
-  box-shadow: 0px 0px 16px rgba(234, 6, 6, 0.25); ;
-  background-color: rgba(225,225,225,0.75);
-  border-radius: 16px 0px 16px 0px;
-}
 h1{
   margin-top: 8%;
+  margin-left:-47%;
+  font-size: 30px;
+  color: #627281;
+}
+h2{
+  margin-top: 30%;
   margin-left:-47%;
   font-size: 30px;
   color: #627281;
