129fbfa2c52890285d0b07220669f5c9f6081009
Alaa Taleb <alaat@student.chalmers.se>
2022-09-29 19:47:32 +0200

    add create new collection button

--- client/src/components/Deckbox.vue
+++ client/src/components/Deckbox.vue
@@ -1,21 +1,26 @@
 <template>
-    <div class="box">{{collection.name}}</div>
+  <div>
+    <div class="box">
+      {{ collection.name }}
+    </div>
+  </div>
 </template>
 <script>
 export default {
   name: 'Deckbox',
-  props: ['collection']
+  props: ['collection'],
+  components: {}
 }
 </script>
 <style scoped>
-.box{
+.box {
   margin-left: 11%;
-  width:200px ;
-  height:100px ;
-  border:none;
+  width: 200px;
+  height: 100px;
+  border: none;
   background: transparent;
   position: absolute;
-  box-shadow: 0px 0px 16px rgba(234, 6, 6, 0.25); ;
-  background-color: rgba(225,225,225,0.75);
+  box-shadow: 0px 0px 16px rgba(234, 6, 6, 0.25);
+  background-color: rgba(225, 225, 225, 0.75);
 }
 </style>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,16 +1,23 @@
 <template>
-<div>
-  <Searchbar/>
-  <h1>Need Revision</h1>
-  <h2>Your Collections </h2>
-  <Deckbox v-for="collection in deckCollections.slice(0,3)" :key="collection._id" :collection="collection" />
-</div>
+  <div>
+    <Searchbar />
+    <h1>Need Revision</h1>
+    <h2>Your Collections</h2>
+
+    <Deckbox
+      v-for="collection in deckCollections.slice(0, 3)"
+      :key="collection._id"
+      :collection="collection"
+    />
+    <createCollection />
+  </div>
 </template>
 
 <script>
 import { Api } from '../Api'
 import Searchbar from '../components/Searchbar.vue'
 import Deckbox from '../components/Deckbox.vue'
+import createCollection from '../components/createCollection.vue'
 export default {
   data() {
     return {
@@ -18,29 +25,34 @@ export default {
     }
   },
   name: 'Dashboard',
-  methods: {},
+  methods: {
+    showModel() {}
+  },
   components: {
+    createCollection,
     Searchbar,
     Deckbox
   },
   mounted: function () {
     Api.get('/collections')
-      .then(res => { this.deckCollections = res.data })
-      .catch(err => console.log(err))
+      .then((res) => {
+        this.deckCollections = res.data
+      })
+      .catch((err) => console.log(err))
   }
 }
 </script>
 
 <style scoped>
-h1{
+h1 {
   margin-top: 8%;
-  margin-left:-47%;
+  margin-left: -47%;
   font-size: 30px;
   color: #627281;
 }
-h2{
+h2 {
   margin-top: 30%;
-  margin-left:-47%;
+  margin-left: -47%;
   font-size: 30px;
   color: #627281;
 }
