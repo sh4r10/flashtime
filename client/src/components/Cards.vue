6a71ce49ddf6e328c63bbdde73b51079ff33fa49
Umar Mahmood <umarma@net.chalmers.se>
2022-09-29 23:23:55 +0200

    add header on card

--- client/src/components/Cards.vue
+++ client/src/components/Cards.vue
@@ -1,7 +1,11 @@
 <template>
     <div>
-        <b-card>
-            {{card.front}}
+        <b-card
+        border-variant="primary"
+        :header="card.front"
+        header-bg-variant="primary"
+        header-text-variant="white"
+      >
         <b-card-text>{{card.back}}</b-card-text>
       </b-card>
     </div>
