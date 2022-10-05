3c9f3556e371487b56e62c24181c7b2c26c51364
Umar Mahmood <umarma@net.chalmers.se>
2022-10-05 16:33:02 +0200

    add page to show all the decks

---
+++ client/src/views/DeckPage.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <Navbar/>
+        <DeckCard v-for="deck in decks" :key="deck._id" :deck="deck" />
+    </div>
+</template>
+<script>
+import Navbar from '../components/Navbar.vue'
+import DeckCard from '../components/DeckCard.vue'
+import { Api } from '../Api'
+
+export default {
+  data() {
+    return {
+      decks: []
+    }
+  },
+  components: {
+    Navbar, DeckCard
+  },
+  mounted: function () {
+    Api.get('/decks')
+      .then((res) => (this.decks = res.data))
+      .catch((err) => console.log(err))
+  }
+
+}
+</script>
+<style scoped>
+</style>
