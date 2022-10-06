d94fbb13aef0152beb714f39989034a9bca2d8aa
Alaa Taleb <alaat@student.chalmers.se>
2022-10-06 19:47:07 +0200

    resolve merging comments

--- client/src/views/Decks.vue
+++ client/src/views/Decks.vue
@@ -10,6 +10,9 @@
       @removeDeck="removeDeck"
     />
     <AddNewDeck :decks="decksToAdd" />
+    <div class="box">
+      <RemoveDeckFromCollection class="button" />
+    </div>
   </div>
 </template>
 <script>
@@ -59,4 +62,21 @@ export default {
   }
 }
 </script>
-<style scoped></style>
+<style scoped>
+.box {
+  margin-left: 11%;
+  width: 200px;
+  height: 100px;
+  border: none;
+  background: transparent;
+  box-shadow: 0px 0px 16px rgba(234, 6, 6, 0.25);
+  background-color: rgba(225, 225, 225, 0.75);
+  text-decoration: none;
+}
+.button {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  text-align: right;
+}
+</style>
