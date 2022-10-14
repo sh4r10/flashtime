1efe9626a70ad37a190245b0203f5b7c214ade00
Shariq Shahbaz <iamsh4r10@gmail.com>
2022-10-14 03:09:28 +0200

    basic image setup

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
   <b-container fluid>
     <Navbar />
-    <b-container fluid>
+    <b-container fluid class="home-container">
       <h1>The <span>Modern</span> Solution to Flashcards</h1>
       <p>Based on the scientifically proven spaced repitition method.</p>
-      <router-link to="/signup"
-        ><b-button variant="primary">Sign up</b-button></router-link
-      >
-      <router-link to="/login"
-        ><b-button variant="secondary">Log in</b-button></router-link
-      >
+      <div class="btn-container">
+        <router-link to="/signup"
+          ><b-button variant="primary">Sign up</b-button></router-link
+        >
+        <router-link to="/login"
+          ><b-button variant="secondary">Log in</b-button></router-link
+        >
+      </div>
+      <div class="image-container">
+        <img src="../assets/images/home-bg.svg"/>
+      </div>
     </b-container>
   </b-container>
 </template>
@@ -22,4 +27,26 @@ export default {
 }
 </script>
 
-<style></style>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&family=Raleway&display=swap');
+
+.home-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
+
+.image-container {
+  width: 100%;
+  margin-top: 5rem;
+  align-self: flex-end
+}
+
+.image-container img{
+  width: 30%;
+  min-width: 400px;
+}
+</style>
