5facb89ceb89301de99e3fae7542f0068c3d9791
Shariq Shahbaz <iamsh4r10@gmail.com>
2022-10-18 18:58:49 +0200

    disallow icons on toastify

--- client/src/main.js
+++ client/src/main.js
@@ -12,7 +12,7 @@ Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.use(VueToastify, {
   errorDuration: 6000,
-  iconEnabled: true,
+  iconEnabled: false,
   canPause: false,
   theme: 'light'
 })
